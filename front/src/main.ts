f5f56c44f7c1e3add046c34333060e2eb853941b
宋诚 <lsjhtwl@163.com>
2026-04-13 11:49:39 +0800

    fix: 修复组件英文问题

--- front/src/main.ts
+++ front/src/main.ts
@@ -6,7 +6,12 @@ import 'ant-design-vue/dist/reset.css'
 import { createPinia } from 'pinia'
 import { router } from './router'
 
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+
 import './style.css'
+
+dayjs.locale('zh-cn')
 
 const app = createApp(App)
 
